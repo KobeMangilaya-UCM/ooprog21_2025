e9069524c4d598c1813d898e5b192655fc2f6eb6
KobeMangilaya-UCM <resetoandy@gmail.com>
2025-09-15 10:22:13 +0800

    Update Companyinfo.java

--- Chapter 3/Companyinfo.java
+++ Chapter 3/Companyinfo.java
@@ -1,1 +1,15 @@
-
+public class CompanyInfo
+{  
+      public static void main(String[] args)
+      {
+         displayHours();    
+      }
+ 
+      public static void displayHours(){
+         System.out.println("Smart Electronics\n");
+         System.out.println("Monday - Friday 8 am to 6 pm\n");
+         System.out.println("Saturday        8 am to noon\n");
+         System.out.println("Sunday          closed\n");
+ 
+      }
+}
