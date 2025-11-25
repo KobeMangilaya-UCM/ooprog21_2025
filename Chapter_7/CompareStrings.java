56a9e5bb8e91fce358247e16c23535c5d3791ce4
KobeMangilaya-UCM <resetoandy@gmail.com>
2025-11-25 18:18:37 +0800

    Create CompareStrings.java

---
+++ Chapter_7/CompareStrings.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CompareStrings {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your name > ");
+        String userInput = input.nextLine();
+
+        String predefined = "Carmen";
+
+        if (userInput.equals(predefined)) {
+            System.out.println("Carmen equals Carmen");
+        } else {
+            System.out.println("Carmen does not equal " + userInput);
+        }
+
+        input.close();
+    }
+}
