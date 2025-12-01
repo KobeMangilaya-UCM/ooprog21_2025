cd1d82bd7154992ca45324a9e5cc2e842a114ad3
KobeMangilaya-UCM <resetoandy@gmail.com>
2025-12-01 12:08:57 +0800

    Add ArraysDemo class to demonstrate array operations

---
+++ Chapter_8/ArraysDemo.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class ArraysDemo {
+    
+    public static void main(String[] args) {
+        int[] array = new int[5];
+        
+        display("Original array:", array);
+        
+        Arrays.fill(array, 8);
+        display("After filling with 8s:", array);
+        
+        array[2] = 6;
+        array[4] = 3;
+        display("After changing two values:", array);
+        
+        Arrays.sort(array);
+        display("After sorting:", array);
+    }
+    
+    public static void display(String message, int[] array) {
+        System.out.print(message + "\t\t");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+}
